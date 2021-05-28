2acc5e052e637fef3f5b86f9374caf95e9d23fc2
davisstacked <brettzvi@gmail.com>
2021-05-27 18:06:18 -0700

    added Don't have an account div - readded form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
   return (
     <div
       className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="flex w-3/5 mr-10">
         <img
           className="rounded-3xl max-w-full"
           src="/images/treehugger.jpeg"
@@ -35,6 +35,34 @@ const Login = () => {
             alt="TreeHugger Logo" />
         </h1>
         {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            onChange={({target}) => setEmailAddress(target.value)}
+          />
+          <input
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            onChange={({target}) => setPassword(target.value)}
+          />
+          <button
+            disable={isInvalid}
+            type="submit"
+            // need to put isInvalid on separate line or it won't read correctly
+            className={`bg-green-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && 'opacity-50'}`}
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+      <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+        <p className="text-sm">Don't have an account? {` `}</p>
       </div>
     </div>
   )
