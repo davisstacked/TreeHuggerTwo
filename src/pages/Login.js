df2a062395ee1b8ae7605cc40f187a4ceab27075
davisstacked <brettzvi@gmail.com>
2021-05-28 10:48:14 -0700

    customized color pallete with tailwindCSS and styled Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import FirebaseContext from '../context/firebase';
 
 const Login = () => {
@@ -15,54 +15,63 @@ const Login = () => {
   const handleLogin = () => { };
 
   useEffect(() => {
-    document.title = 'Login - TreeHugger';
+    document.title = 'Login - Tree Hugger';
   }, []);
 
   return (
     <div
-      className="container flex mx-auto max-w-screen-md items-center h-screen">
+      className="container flex mx-auto max-w-screen-lg items-center h-screen bg-tan-100 border border-green-100 px-6">
       <div className="flex w-3/5 mr-10">
         <img
-          className="rounded-3xl max-w-full"
+          className="rounded-xl max-w-full"
           src="/images/treehugger.jpeg"
           alt="treehugger logo" />
       </div>
       <div className="flex flex-col w-2 w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img
-            className="mt-2 w-6/12 mb-4 rounded-3xl"
-            src="/images/logo.png"
-            alt="TreeHugger Logo" />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            onChange={({target}) => setEmailAddress(target.value)}
-          />
-          <input
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            onChange={({target}) => setPassword(target.value)}
-          />
-          <button
-            disable={isInvalid}
-            type="submit"
-            // need to put isInvalid on separate line or it won't read correctly
-            className={`bg-green-500 text-white w-full rounded h-8 font-bold
-            ${isInvalid && 'opacity-50'}`}
-          >
-            Log In
-          </button>
-        </form>
-      </div>
-      <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
-        <p className="text-sm">Don't have an account? {` `}</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-green-200 mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              className="mt-2 w-6/12 mb-4 rounded-3xl"
+              src="/images/logo.png"
+              alt="TreeHugger Logo" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-green-200 rounded mb-2"
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              onChange={({target}) => setEmailAddress(target.value)}
+            />
+            <input
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-green-200 rounded mb-2"
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              onChange={({target}) => setPassword(target.value)}
+            />
+            <button
+              disable={isInvalid}
+              type="submit"
+              // need to put isInvalid on separate line or it won't read correctly
+              className={`bg-green-500 rounded text-white w-full rounded border border-green-600 h-8 font-bold
+              ${isInvalid && 'opacity-50'}`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex rounded justify-center items-center flex-col w-full bg-white p-4 border border-green-200">
+          <p className="text-sm">Don't have an account? {` `}
+            <Link
+              to="/signup"
+              className="font-bold text-green-500"
+            >
+              Sign up
+            </Link>
+          </p>
+          </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          '900': '#071207',
+          '800': '#163716',
+          '700': '#2c6d2d',
+          '600': '#41a443',
+          '500': '#64c166',
+          '400': '#7fcc81',
+          '200': '#a4dba5',
+          '100': '#dbf0db'
+        },
+        'tan-100': '#f8f3ed',
+        'tan-200': '#eadf77',
+        'tan-300': '#d2b48c',
+        'tan-400': '#bf935a',
+        'tan-500': '#815e32',
+        'tan-600': '#5c4324',
+        'tan-700': '#49361d',
+        'tan-800': '#251b0e',
+        'tan-900': '#120d07',
+      }
+    },
   },
   variants: {
     extend: {},
