47d6ecd1eab8edaee7acb8c9a1c41030b84af699
davisstacked <brettzvi@gmail.com>
2021-08-17 12:35:56 -0700

    Not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import('./pages/Login'));
 const SignUp = lazy(() => import('./pages/SignUp'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route component={NotFound} />
           </Switch>
         </Suspense>
       </Router>
